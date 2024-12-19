af4a7b65559d86a73255cc7889f6dddb287b522b
RostiMelk <hello@rosti.no>
2024-12-19 20:42:14 +0100

    feat: remove dist

--- dist/cli.cjs
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-var index = require("./index.cjs");
-const directory = process.argv[2] || process.cwd();
-index.validateLocal(directory);
-//# sourceMappingURL=cli.cjs.map
