a20f130b2d97b16d8b3c15d3c7f10539ce5ca0e2
RostiMelk <hello@rosti.no>
2025-01-08 14:26:04 +0100

    feat: revert

--- test/constants.ts
+++ test/constants.ts
@@ -7,5 +7,4 @@ const __dirname = dirname(__filename)
 const branch = github?.context?.payload?.pull_request?.head?.ref ?? 'main'
 
 export const LOCAL_FIXTURES = join(__dirname, 'fixtures')
-// export const REMOTE_FIXTURES = `https://raw.githubusercontent.com/sanity-io/template-validator/${branch}/test/fixtures`
-export const REMOTE_FIXTURES = `https://raw.githubusercontent.com/sanity-io/template-validator/feat/get-monorepo-wildcard-directories/test/fixtures`
+export const REMOTE_FIXTURES = `https://raw.githubusercontent.com/sanity-io/template-validator/${branch}/test/fixtures`
