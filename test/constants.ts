9148f418ca7892560d3d7de7bdbee071d745b982
RostiMelk <hello@rosti.no>
2025-01-08 14:29:01 +0100

    feat: tests should always test main

--- test/constants.ts
+++ test/constants.ts
@@ -1,10 +1,9 @@
 import {fileURLToPath} from 'node:url'
 import {join, dirname} from 'node:path'
-import * as github from '@actions/github'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
-const branch = github?.context?.payload?.pull_request?.head?.ref ?? 'main'
 
 export const LOCAL_FIXTURES = join(__dirname, 'fixtures')
-export const REMOTE_FIXTURES = `https://raw.githubusercontent.com/sanity-io/template-validator/${branch}/test/fixtures`
+export const REMOTE_FIXTURES =
+  'https://raw.githubusercontent.com/sanity-io/template-validator/main/test/fixtures'
