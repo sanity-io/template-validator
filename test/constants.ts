2a5fd897bbe24672a4810249741af100dcda3bb6
RostiMelk <hello@rosti.no>
2025-01-08 13:39:35 +0100

    feat: test against ref

--- test/constants.ts
+++ test/constants.ts
@@ -1,9 +1,11 @@
 import {fileURLToPath} from 'node:url'
 import {join, dirname} from 'node:path'
+import * as github from '@actions/github'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
+const branch = github?.context?.ref?.replace('refs/heads/', '') ?? 'main'
+
 export const LOCAL_FIXTURES = join(__dirname, 'fixtures')
-export const REMOTE_FIXTURES =
-  'https://raw.githubusercontent.com/sanity-io/template-validator/main/test/fixtures'
+export const REMOTE_FIXTURES = `https://raw.githubusercontent.com/sanity-io/template-validator/${branch}/test/fixtures`
