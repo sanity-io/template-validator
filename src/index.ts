6f13a744aa5bae6761035c0b1255bf3d5889e46c
Gustav Hansen <gustav.hansen@sanity.io>
2026-03-19 13:42:20 -0700

    feat: allow remote sdk app templates

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 export {validateLocalTemplate} from './local'
 export {validateRemoteTemplate} from './remote'
-export {ENV_FILE, ENV_TEMPLATE_FILES, REQUIRED_ENV_VAR} from './utils/constants'
+export {
+  ENV_FILE,
+  ENV_TEMPLATE_FILES,
+  REQUIRED_ENV_VAR_STUDIO as REQUIRED_ENV_VAR,
+  REQUIRED_ENV_VAR_APP,
+} from './utils/constants'
 export {type FileReader, GitHubFileReader, LocalFileReader} from './utils/fileReader'
 export type {ValidationResult} from './utils/types'
 export {getMonoRepo, validateTemplate} from './utils/validator'
